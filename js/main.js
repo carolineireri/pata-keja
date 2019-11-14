263c9a17cd0b400b18cd43ee2a568d6a26cffc7f
carolineireri <carolnyamburaireri@gmail.com>
2019-11-14 14:26:47 +0300

    Rearranged code in js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-$("#contact").submit(function(event) {
-    event.preventDefault();
-    var blanks = ["name", "email", "message"];
-    var input = [];
-    blanks.forEach(function(blank) {
-      input.push($("#" + blank).val());
-    });
-    alert(
-      "Thank you for reaching out to us. Your message has been received."
-    );
-    $("#contact")[0].reset();
-  });
 
   //dark mode
 
@@ -26,3 +14,19 @@ $("#contact").submit(function(event) {
       $("body").addClass("day-mode");
     });
   });
+
+  //feedback form 
+
+  $("#contact").submit(function(event) {
+    event.preventDefault();
+    var blanks = ["name", "email", "message"];
+    var input = [];
+    blanks.forEach(function(blank) {
+      input.push($("#" + blank).val());
+    });
+    alert(
+      "Thank you for reaching out to us. Your message has been received."
+    );
+    $("#contact")[0].reset();
+  });
+
