b1c06fa5ce79d2eb36a10dc97004e258bd77c43d
carolineireri <carolnyamburaireri@gmail.com>
2019-11-14 15:23:05 +0300

    Added styles to buttons

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
 
+  
+  //feedback form 
+
+  $("#contact").submit(function(event) {
+    event.preventDefault();
+    var blanks = ["name", "email", "message"];
+    var input = [];
+    blanks.forEach(function(blank) {
+      input.push($("#" + blank).val());
+    });
+    alert(
+      "Thank you for reaching out to us. Your message has been received."
+    );
+    $("#contact")[0].reset();
+  });
+  
   //dark mode
 
   $( ".inner-switch" ).on("click", function() {
