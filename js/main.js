fd37e691b7e4906c0d8b9d600d978278fecc15a1
carolineireri <carolnyamburaireri@gmail.com>
2019-11-15 08:47:05 +0300

    Added propmt alerts

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 
-  
+
+
   //feedback form 
 
-  $("#contact").submit(function(event) {
+  $("#contact").submit(function (event) {
     event.preventDefault();
     var blanks = ["name", "email", "message"];
     var input = [];
-    blanks.forEach(function(blank) {
+    blanks.forEach(function (blank) {
       input.push($("#" + blank).val());
     });
     alert(
@@ -14,15 +15,32 @@
     );
     $("#contact")[0].reset();
   });
-  
+
   //dark mode
 
-  $( ".inner-switch" ).on("click", function() {
-    if( $( "body" ).hasClass( "dark-mode" )) {
-      $( "body" ).removeClass( "dark-mode" );
-      $( ".inner-switch" ).text( "OFF" );
+  $(".inner-switch").on("click", function () {
+    if ($("body").hasClass("dark-mode")) {
+      $("body").removeClass("dark-mode");
+      $(".inner-switch").text("OFF");
     } else {
-      $( "body" ).addClass( "dark-mode" );
-      $( ".inner-switch" ).text( "ON" );
+      $("body").addClass("dark-mode");
+      $(".inner-switch").text("ON");
     }
+
+  });
+
+  //Book site view
+  $("document").ready(function () {
+  $(".book-site").click(function () {
+    var name = prompt(
+      "PLease enter your name"
+    );
+    if (name) {
+      var mobile = prompt("Please enter your mobile number");
+      if (mobile) {
+        alert("Thank you for contacting Pata Keja. We will get back to you soon."
+      );
+    }
+  }
 });
+  });
